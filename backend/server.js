c96a5cf8770ea6527df90c61c1f2c8cf555cf4b1
Mandara-samanalee <samanaleerktm.21@uom.lk>
2024-10-27 18:53:25 +0530

    create database connection

--- backend/server.js
+++ backend/server.js
@@ -3,23 +3,20 @@ const app = express(); //here app is the instance of express
 const cors = require('cors'); //import cors
 const router = require('./router') //import router
 
-const port = 3001; 
+const port = 3001;
 const host = '127.0.0.1'
 const mongoose = require('mongoose'); //import mongoose
+const dotenv = require('dotenv');
+dotenv.config();
 
-
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 
-
-
-//connect to the database
-const dbURI = "mongodb+srv://Mandara:mandara123@cluster1.kxf4j.mongodb.net/?retryWrites=true&w=majority&appName=cluster1";
-
+const dbURL = process.env.dbURL;
 
 const connect = async () => {
     try {
-        await mongoose.connect(dbURI);
+        await mongoose.connect(dbURL);
         console.log('Successfully connected to the Mongodb');
     } catch (error) {
         console.log('Error connecting to the Mongodb', error);
